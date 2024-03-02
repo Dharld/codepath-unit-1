779687d6cd4d22333cc3213181d2923b70cf3743
Yann Djoumessi <yanndjoumessi@gmail.com>
2024-03-02 01:48:40 -0500

    Dynamising the application

--- unit-1/src/App.jsx
+++ unit-1/src/App.jsx
@@ -1,86 +1,130 @@
 import './App.scss';
-import leagueIcon from './assets/icons/leagueIcon@2x.png';
-import teamIcon from "./assets/icons/inter@2x.png";
-import teamBigIcon from "./assets/icons/inter@3x.png";
 import { League } from './components/league/league.component';
 import { Team } from './components/team/team.component';
 import Standing from './components/standing/standing.component';
 import Scorers from './components/scorers/scorers.component';
 import Fixture from './components/fixture/fixture.component';
+import { useEffect, useState } from 'react';
+import { countries } from './assets/data.js';
+import axios from 'axios';
 
-const teams = new Array(20).fill({
-  rank: 1,
-  name: "Inter Milan",
-  w: 5,
-  l: 4,
-  d: 3,
-  gd: 2,
-  pts: 1,
-  icon: teamIcon
-});
-
-const scorers = new Array(5).fill({
-  rank: 1,
-  player: "Lautaro",
-  club: "Inter Milan XXXXXXX",
-  goals: 35
-});
-
-const fixtures = new Array(15).fill({
-  league: "Serie A",
-  week: 23,
-  date: "2020-01-01",
-  home: {
-    team: "Inter Milan",
-    icon: teamBigIcon
-  },
-  away: {
-    team: "Inter Milan",
-    icon: teamBigIcon
-  }
-})
+const apiKey = '114a3bb49ad418ed3a0332b1b5547f3ecaf51cf3cc7ed6508064bc49f545aa17';
+const INITIAL_STATE = {
+  leagues: [],
+  fixtures: [],
+  standings: [],
+  scorers: [],
+  teams: [],
+  activeLeague: null,
+  activeTeam: null,
+  isLoadingLeague: true,
+  isLoadingTeam: true,
+  isLoadingStanding: false,
+  isLoadingScorers: true,
+};
 function App() {
+  const [state, setState] = useState(INITIAL_STATE);
+
+  useEffect(() => {
+    const leagues = countries.map(({ league: { league_id, league_name, league_season, league_logo, teams } }) => {
+      return {
+        league_id,
+        league_name,
+        league_season,
+        league_logo,
+        teams,
+      };
+    });
+
+    const activeLeague = leagues[0];
+    const teams = activeLeague.teams;
+    const activeTeam = leagues[0].teams[0];
+
+    setState((state) => ({
+      ...state,
+      teams,
+      leagues,
+      activeTeam,
+      activeLeague,
+      isLoadingTeam: false,
+      isLoadingLeague: false,
+    }));
+  }, []);
+
+  useEffect(() => {
+    const getStanding = async (leagueId) => {
+      try {
+        const url = `https://apiv3.apifootball.com/?action=get_standings&league_id=${leagueId}&APIkey=${apiKey}`;
+        const { data } = await axios.get(url);
+
+        console.log(data);
+
+        const standings = data.map((d) => ({
+          name: d.team_name,
+          rank: d.overall_league_position,
+          l: d.overall_league_L,
+          w: d.overall_league_W,
+          d: d.overall_league_D,
+          pts: d.overall_league_PTS,
+          gd: +d.overall_league_GF - +d.overall_league_GA,
+        }));
+
+        setState((state) => ({
+          ...state,
+          standings,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (!state.activeLeague) {
+      return;
+    }
+
+    getStanding(state.activeLeague.league_id);
+  }, [state.activeLeague]);
+
+  const changeActiveLeague = (league) => {
+    setState((state) => ({
+      ...state,
+      activeLeague: league,
+    }));
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <div className="container">
         <div className="left col--3 bg-component">
-          <h3 className='title'>Regional Leagues</h3>
+          <h3 className="title">Regional Leagues</h3>
           <div className="leagues">
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
-            <League name="Serie A Italian" teamNumber={20} logo={leagueIcon}/>
+            {state.isLoadingLeague ? (
+              <div className="spinner"></div>
+            ) : (
+              state.leagues.map((l, i) => (
+                <League key={i} name={l.league_name} logo={l.league_logo} onClick={() => changeActiveLeague(l)} />
+              ))
+            )}
           </div>
-          <h3 className='title'>Regional Leagues</h3>
+          <h3 className="title">Regional Leagues</h3>
           <div className="teams">
-            <Team name="Inter Milan" icon={teamIcon}/>
-            <Team name="Inter Milan" icon={teamIcon}/>
-            <Team name="Inter Milan" icon={teamIcon}/>
-            <Team name="Inter Milan" icon={teamIcon}/>
-            <Team name="Inter Milan" icon={teamIcon}/>
+            {state.isLoadingTeam ? (
+              <div className="spinner"></div>
+            ) : (
+              state.teams.map((t, i) => <Team key={i} name={t.team_name} teamNumber={20} icon={t.team_badge} />)
+            )}
           </div>
         </div>
-        <main className='main bg-component'>
-          <div className="fixtures">
-            { fixtures.map((f, i) => <Fixture key={i} {...f}/>)}
-           
-          </div>
+        <main className="main bg-component">
+          <div className="fixtures">{state.fixtures && state.fixtures.map((f, i) => <Fixture key={i} {...f} />)}</div>
         </main>
         <div className="right  col--3">
-          <div className="space bg-component">
-            <Standing teams={teams}/>
-          </div>
-          <div className="second space bg-component">
-            <Scorers scorers={scorers}/>
-          </div>
+          <div className="space bg-component">{state.standings && <Standing teams={state.standings} />}</div>
+          <div className="second space bg-component">{state.scorers && <Scorers scorers={state.scorers} />}</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- unit-1/src/components/league/league.component.jsx
+++ unit-1/src/components/league/league.component.jsx
@@ -1,15 +1,13 @@
 /* eslint-disable react/prop-types */
+import './league.styles.scss';
 
-import "./league.styles.scss";
-
-export const League = ({ logo, name, teamNumber }) => {
+export const League = ({ logo, name, onClick }) => {
   return (
-    <div className="league flex">
-        <div className="badge">
-            <img src={logo} alt="" />
-        </div>
-        <div className="name">{ name}</div>
-        <div className="teamNumber">{teamNumber}</div>
+    <div className="league flex" onClick={onClick}>
+      <div className="badge">
+        <img src={logo} alt="" />
+      </div>
+      <div className="name">{name}</div>
     </div>
-  )
-}
+  );
+};
